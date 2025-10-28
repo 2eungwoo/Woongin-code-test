cc772cc447d64f1972c8c7c8d671044836da4ea2
swlee <swo98@naver.com>
2025-10-28 23:45:14 +0900

    fix: 기존의 runtimeException 핸들러와 로그 스타일을 일치시킴

--- src/main/java/com/wjc/codetest/product/global/GlobalExceptionHandler.java
+++ src/main/java/com/wjc/codetest/product/global/GlobalExceptionHandler.java
@@ -18,8 +18,9 @@ public class GlobalExceptionHandler {
     @ResponseBody // 도메인별 예외 중 커스텀예외를 상속받은 예외클래스 경우
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<String> handleCustomException(CustomException e) {
-        log.error("status:: {}, message :: {}",
+        log.error("status:: {}, errorType :: {}, errorCause :: {}",
             e.getHttpStatus(),
+            "customException",
             e.getMessage()
         );
 
@@ -31,39 +32,29 @@ public class GlobalExceptionHandler {
     @ResponseBody // '@Valid' '@RequestBody' 에서 검증 예외
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult().getFieldErrors().stream()
-            .map(error -> String.format("[%s] %s", error.getField(), error.getDefaultMessage()))
-            .findFirst()
-            .orElse("잘못된 요청 값입니다.");
-
         log.error("status :: {}, errorType :: {}, errorCause :: {}",
             HttpStatus.BAD_REQUEST,
             "methodArgumentNotValidException",
-            message
+            e.getMessage()
         );
 
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
-            .body(message);
+            .body("Bad Request");
     }
 
     @ResponseBody // 요청 본문 json 불일치 예외
     @ExceptionHandler(BindException.class)
     public ResponseEntity<String> handleBindException(BindException e) {
-        String message = e.getBindingResult().getFieldErrors().stream()
-            .map(error -> String.format("[%s] %s", error.getField(), error.getDefaultMessage()))
-            .findFirst()
-            .orElse("요청 바인딩 오류입니다.");
-
         log.error("status :: {}, errorType :: {}, errorCause :: {}",
             HttpStatus.BAD_REQUEST,
             "bindException",
-            message
+            e.getMessage()
         );
 
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
-            .body(message);
+            .body("Bad Request");
     }
 
     @ResponseBody // 요청 파라미터 잘못 됐을 때 예외
@@ -77,7 +68,7 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
-            .body(e.getMessage());
+            .body("Bad Request");
     }
 
     @ResponseBody // 파라미터 범위 안맞거나 null, 음수 등의 예외 경우
@@ -91,7 +82,7 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity
             .status(HttpStatus.BAD_REQUEST)
-            .body(e.getMessage());
+            .body("Bad Request");
     }
 
     @ResponseBody // 그 외 나머지 예외
