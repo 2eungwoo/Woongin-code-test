affc97e9512fdc191c39bc170bee08e4af2b3e62
swlee <swo98@naver.com>
2025-10-28 19:29:26 +0900

    fix: put 컨트롤러에 작업해야할 내용을 실수로 get에 한거 수정

--- src/main/java/com/wjc/codetest/product/controller/ProductController.java
+++ src/main/java/com/wjc/codetest/product/controller/ProductController.java
@@ -38,17 +38,15 @@ public class ProductController {
     }
 
     @PutMapping(value = "/products/{productId}")
-    public ResponseEntity<Product> updateProductById(@RequestBody UpdateProductRequest dto){
-        Product product = productService.updateProductById(dto);
+    public ResponseEntity<Product> updateProductById(@PathVariable(name = "productId") Long productId,
+                                                     @RequestBody UpdateProductRequest dto) {
+        Product product = productService.updateProductById(productId, dto);
         return ResponseEntity.ok(product);
     }
 
     @GetMapping(value = "/products")
-    public ResponseEntity<ProductListResponse> getProductListByCategory(
-        @PathVariable(name = "productId") Long productId,
-        @RequestBody GetProductListRequest dto)
-    {
-        Page<Product> productList = productService.getProductListByCategory(productId, dto);
+    public ResponseEntity<ProductListResponse> getProductListByCategory(@RequestBody GetProductListRequest dto) {
+        Page<Product> productList = productService.getProductListByCategory(dto);
         return ResponseEntity.ok(new ProductListResponse(productList.getContent(), productList.getTotalPages(), productList.getTotalElements(), productList.getNumber()));
     }
 
